4d1f0cb786593c362926b1105c8c9adfe2f4f775
Rob van Son <r.j.j.h.vanson@gmail.com>
2026-07-06 15:00:48 +0200

    Create installWhisperInPraat.sh
    small script to install a whisper model for Praat

---
+++ Scripts/installWhisperInPraat.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/Library/Preferences/Praat\ Prefs/models/whispercpp/
+cd ~/Library/Preferences/Praat\ Prefs/models/whispercpp/
+wget https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-tiny-q5_1.bin
+cd -
